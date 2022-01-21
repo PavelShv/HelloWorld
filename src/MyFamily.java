fa62c0f2ff6bb1131bd360af55a1dda5473bc0d5
pavel.shvedov <composer.shvedov@gmail.com>
2022-01-21 11:35:01 +0300

    List of names in my family

---
+++ src/MyFamily.java
@@ -0,0 +1,14 @@
+public class MyFamily {
+    public static void main(String[] args) {
+        System.out.println("Here are the names in my family: ");
+        System.out.println("Pavel");
+        System.out.println("Alena");
+        System.out.println("Julia");
+        System.out.println("Jacob");
+        System.out.println("Sergey");
+        System.out.println("Liana");
+        System.out.println("Mikhail");
+        System.out.println("Elena");
+        System.out.println("Christian");
+    }
+}
